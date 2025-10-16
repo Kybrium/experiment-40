5df6a6333d770aaa0f77b2137d678846bac3bd45
Max <maxchergik@gmail.com>
2025-10-16 17:23:38 +0300

    feat(backend): added MaintenanceMiddleware

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,6 +25,7 @@ HOSTS_URLS = os.getenv("DJANGO_HOSTS_URLS","http://localhost:3000,http://127.0.0
 ALLOWED_HOSTS = HOSTS
 CORS_ALLOWED_ORIGINS = HOSTS_URLS
 CSRF_TRUSTED_ORIGINS = HOSTS_URLS
+MAINTENANCE_MODE = os.getenv('MAINTENANCE_MODE') == 'True'
 # ===================================
 
 
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'core.middlewares.MaintenanceModeMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
