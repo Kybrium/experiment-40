a92d4ff151e56aee3432e44b5fdffe8a7ff858fc
Max <maxchergik@gmail.com>
2025-10-28 16:31:10 +0200

    feat(backend): updated minecraft account model

--- backend/minecraft/models.py
+++ backend/minecraft/models.py
@@ -5,8 +5,13 @@ class MinecraftAccount(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     uuid = models.CharField(max_length=64, unique=True)
     token = models.OneToOneField(GameToken, on_delete=models.CASCADE, related_name='minecraft_account')
+
     is_dead = models.BooleanField(default=False)
+    dead_at = models.DateTimeField(blank=True, null=True)
+
     is_active = models.BooleanField(default=True)
+    deactivated_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nickname
